082bfa80f65f3f087168f4dff68c4f5db91efc1b
Badih Barakat <88603442+badih76@users.noreply.github.com>
2025-03-08 14:26:09 +1100

    Add the website in fms folder

---
+++ fms/precache-manifest.27b8b6e88b89b0e272e0c6b02a354e67.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "a94cee7e5d980c75ba73",
+    "url": "/fms/static/css/main.1ded4630.chunk.css"
+  },
+  {
+    "revision": "a94cee7e5d980c75ba73",
+    "url": "/fms/static/js/main.a94cee7e.chunk.js"
+  },
+  {
+    "revision": "c9faa2c3142736fb5cf6",
+    "url": "/fms/static/css/1.ae1fbe68.chunk.css"
+  },
+  {
+    "revision": "c9faa2c3142736fb5cf6",
+    "url": "/fms/static/js/1.c9faa2c3.chunk.js"
+  },
+  {
+    "revision": "2fbb6632d007817dfa33",
+    "url": "/fms/static/js/runtime~main.2fbb6632.js"
+  },
+  {
+    "revision": "a6e3d85f3436bbea68a0cc53fd037296",
+    "url": "/fms/static/media/processing.a6e3d85f.gif"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/fms/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/fms/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/fms/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/fms/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/fms/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "cb5b10639daf27ffe14b9be50be7fc02",
+    "url": "/fms/index.html"
+  }
+];
